9c8a68eb30d41fce8ada9179d13d7a2d17bcac7e
smanikandan14 <smanikandan14@gmail.com>
2014-12-01 23:24:05 -0600

    More.

--- app/src/main/java/mani/lollipopexperiment/RippleExampleActivity.java
+++ app/src/main/java/mani/lollipopexperiment/RippleExampleActivity.java
@@ -4,14 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 /**
- * Created by maniselvaraj on 29/11/14.
+ * Created by maniselvaraj on 1/12/14.
  */
-public class MyActivity1 extends ActionBarActivity {
+public class RippleExampleActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.act2);
-        //getWindow().setStatusBarColor(Color.BLUE);
+        setContentView(R.layout.activity_allwidgets);
+
+
     }
 }
