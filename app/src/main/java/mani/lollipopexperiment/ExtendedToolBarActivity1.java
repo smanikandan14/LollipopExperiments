9eae0ab8b320f7d02d9c7b6e506d905a56970253
smanikandan14 <smanikandan14@gmail.com>
2014-12-07 23:41:28 -0600

    More updates.

--- app/src/main/java/mani/lollipopexperiment/ExtendedToolBarActivity1.java
+++ app/src/main/java/mani/lollipopexperiment/ExtendedToolBarActivity1.java
@@ -3,6 +3,7 @@ package mani.lollipopexperiment;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 /**
  * Created by maniselvaraj on 4/12/14.
@@ -17,7 +18,13 @@ public class ExtendedToolBarActivity1 extends ActionBarActivity {
         Toolbar toolBar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolBar);
         getSupportActionBar().setTitle("Title");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.light_toolbar_menu, menu);
+        return true;
     }
 
 }
